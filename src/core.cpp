43c5bd4081f8c5caa2ac2c900098724598958291
jinooLi <jinu1423@naver.com>
2024-10-06 01:42:45 +0900

    제어 목적을 라이다 데이터의 평균 벡터 각도로 변경함

--- src/core.cpp
+++ src/core.cpp
@@ -1,10 +1,13 @@
 #include "../include/car_control/core.hpp"
 
 #include <memory>
+#include <cmath>
 
 const float pi = 3.14159265359;
 const float ratioConst = 0.01;
 const float middleRatio = 0.01;
+const int angle_num = 30;
+const float p_gain = 1;
 
 using namespace core;
 
@@ -18,24 +21,33 @@ ControlCar::ControlCar() {
 
 AckermanOut ControlCar::controlOnce(std::vector<float> laserData) {
     AckermanOut out = AckermanOut();
-
+    
     int frontIndex = angleToIndexInLaser(0);
-    int lIndex = angleToIndexInLaser(pi / 2);
-    int rIndex = angleToIndexInLaser(-pi / 2);
-    int lfIndex = angleToIndexInLaser(pi / 4);
-    int rfIndex = angleToIndexInLaser(-pi / 4);
-
+    
     if (laserData[frontIndex] < 1) {
         out.steering_angle = 0;
         out.velocity = 0;
     } else {
-        float leftRatio = laserData[lfIndex] / laserData[lIndex];
-        float rightRatio = laserData[rfIndex] / laserData[rIndex];
+        float accum_x = 0;
+        float accum_y = 0;
 
-        float error = (leftRatio - rightRatio) * ratioConst +
-                      (laserData[lIndex] - laserData[rIndex]) * middleRatio;
-        out.steering_angle = error * 10;
+        // 라이다 데이터를 이용하여 가야하는 방향을 계산한다.
+        for (int i = 0; i < angle_num; i++) {
+            float angle = -pi / 2 + pi / (angle_num - 1) * i;
+            int index = angleToIndexInLaser(angle);
+            float oneLaserData = laserData[index];
+            if (oneLaserData > 10) {
+                oneLaserData = 10;
+            }
 
+            // 구한 각도와 라이다 데이터를 각 좌표계에서 직교좌표계로 변환하여 다 더한다.
+            accum_x += oneLaserData * cos(angle);  // x 좌표
+            accum_y += oneLaserData * sin(angle);  // y 좌표
+        }
+
+        // 직교좌표계의 x, y 좌표를 이용하여 차량이 가야할 방향의 각도를 구한다.
+        float target_angle = atan2(accum_y, accum_x);
+        out.steering_angle = target_angle*p_gain;
         out.velocity = 2;
     }
 
