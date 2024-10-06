98b1c1ec4c757b71a26a0541a4dd11b48ed5e374
jinoo <jinu1423@naver.com>
2024-10-06 19:58:49 +0000

    Add velocity control

--- src/core.cpp
+++ src/core.cpp
@@ -1,13 +1,13 @@
 #include "../include/car_control/core.hpp"
 
-#include <memory>
 #include <cmath>
+#include <memory>
 
 const float pi = 3.14159265359;
-const float ratioConst = 0.01;
-const float middleRatio = 0.01;
 const int angle_num = 30;
+const float angle_range = 2 * pi / 3;
 const float p_gain = 1;
+const float velocity_gain = 0.5;
 
 using namespace core;
 
@@ -18,12 +18,11 @@ ControlCar::ControlCar() {
     this->controlCycleMs = 10;  // 제어 주기 꼭 설정!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
-
 AckermanOut ControlCar::controlOnce(std::vector<float> laserData) {
     AckermanOut out = AckermanOut();
-    
+
     int frontIndex = angleToIndexInLaser(0);
-    
+
     if (laserData[frontIndex] < 1) {
         out.steering_angle = 0;
         out.velocity = 0;
@@ -33,7 +32,7 @@ AckermanOut ControlCar::controlOnce(std::vector<float> laserData) {
 
         // 라이다 데이터를 이용하여 가야하는 방향을 계산한다.
         for (int i = 0; i < angle_num; i++) {
-            float angle = -pi / 2 + pi / (angle_num - 1) * i;
+            float angle = -angle_range / 2 + angle_range / (angle_num - 1) * i;
             int index = angleToIndexInLaser(angle);
             float oneLaserData = laserData[index];
             if (oneLaserData > 10) {
@@ -45,10 +44,15 @@ AckermanOut ControlCar::controlOnce(std::vector<float> laserData) {
             accum_y += oneLaserData * sin(angle);  // y 좌표
         }
 
+        // accumulate된 x, y 좌표를 angle_num으로 나누어 평균을 구한다.
+        accum_x /= angle_num;
+        accum_y /= angle_num;
+
         // 직교좌표계의 x, y 좌표를 이용하여 차량이 가야할 방향의 각도를 구한다.
         float target_angle = atan2(accum_y, accum_x);
-        out.steering_angle = target_angle*p_gain;
-        out.velocity = 2;
+        out.steering_angle = target_angle * p_gain;
+        // velocity is accumulated x, y's length
+        out.velocity = sqrt(accum_x * accum_x + accum_y * accum_y) * velocity_gain;
     }
 
     out.accel = 4;
